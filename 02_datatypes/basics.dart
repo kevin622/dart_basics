f07cbf4c951586934869c7e5d0c0b2ed1ede8c74
박국현 <duru0622@gmail.com>
2023-07-13 12:26:27 +0900

    doing datatypes

---
+++ 02_datatypes/basics.dart
@@ -0,0 +1,11 @@
+import 'dart:collection';
+
+void main(List<String> args) {
+  String name1 = 'name'; // single quote
+  String name2 = "name"; // double quote
+  bool alive = true;
+  int age = 12;
+  double money = 12.99;
+  num x = 1.1;
+  num y = 1;
+}
---
+++ 02_datatypes/lists.dart
@@ -0,0 +1,7 @@
+void main(List<String> args) {
+  var numbers = [1, 2, 3, 4];
+  List<int> array = [1, 2, 3, 4];
+
+  numbers.add(5);
+  k
+}
